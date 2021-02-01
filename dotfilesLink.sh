73b5d7b99880e2cae0d21f2b25ed856725ccc047
eguchiaoi <eguchi@hep.phys.s.u-tokyo.ac.jp>
2021-02-01 18:11:27 +0900

    Update shell script

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 # Define symbolic links
-ln -sf $HOME/dotfiles/.vimrc $HOME/.vimrc
+ln -sf $HOME/dotfiles/.vimrc        $HOME/.vimrc
 ln -sf $HOME/dotfiles/.bash_profile $HOME/.bash_profile
+ln -sf $HOME/dotfiles/.bashrc       $HOME/.bashrc
+ln -sf $HOME/dotfiles/.profile      $HOME/.profile
 
 
